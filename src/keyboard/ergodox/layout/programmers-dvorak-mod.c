0564b00ce697d3a6735c92ff710c09bbd9baea99
Andrew Sharp <andrew.sharp57@gmail.com>
2016-09-04 13:06:44 -0400

    fix numbers

--- src/keyboard/ergodox/layout/programmers-dvorak-mod.c
+++ src/keyboard/ergodox/layout/programmers-dvorak-mod.c
@@ -25,16 +25,16 @@ const uint8_t PROGMEM _kb_layout[KB_LAYERS][KB_ROWS][KB_COLUMNS] = {
 // unused
 0,
 // left hand
-    _equal,     _1,         _2,      _3,      _4,    _5, _esc,
-_backslash,     _Q,         _W,      _E,      _R,    _T,    1,
-      _tab,     _A,         _S,      _D,      _F,    _G,
+        _1,     _2,         _3,      _4,      _5,    _6, _esc,
+      _tab,     _Q,         _W,      _E,      _R,    _T,    1,
+ _capsLock,     _A,         _S,      _D,      _F,    _G,
    _shiftL,     _Z,         _X,      _C,      _V,    _B,    1,
      _guiL, _grave, _backslash, _arrowL, _arrowR,
                                                  _ctrlL, _altL,
                                               0,      0, _home,
                                             _bs,   _del,  _end,
 // right hand
-        3, _6,      _7,      _8,      _9,         _0,     _dash,
+        3, _7,      _8,      _9,      _0,      _dash,         0,
 _bracketL, _Y,      _U,      _I,      _O,         _P, _bracketR,
            _H,      _J,      _K,      _L, _semicolon,    _quote,
         1, _N,      _M,  _comma, _period,     _slash,   _shiftR,
